dac5680819fcad4631c5a8d0051de01cc0c6e169
Delpreti <rddesiqueira@gmail.com>
2021-04-27 01:06:16 -0300

    reta final

--- trab1/script.sh
+++ trab1/script.sh
@@ -3,7 +3,8 @@
 for difficulty in {1..7}; do
     for threads in {1,2,4,8}; do
         for i in {1..10}; do
-            ./threadmine "$difficulty" "$threads"
+            ./threadmine "$difficulty" "$threads";
+            sleep 1;
         done
     done
 done
